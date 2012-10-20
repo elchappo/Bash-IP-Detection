c8a5146cb2299c6ec896f7f41ef795263624bfa9
Pawel <zareba.pawel@gmail.com>
2012-10-20 23:22:20 +0100

    First commit

---
+++ ip-test.sh
@@ -0,0 +1,42 @@
+#!/bin/bash 
+ 
+NOW=$(date +"%m/%d/%Y")
+
+LOG=${PWD}/"ip.log"
+
+MAIL="/bin/mail"
+
+# email subject
+SUBJECT="SLACK - Latest IP address"
+
+# Email To ?
+EMAIL="zareba.pawel@gmail.com"
+
+# Email text/message
+EMAILMESSAGE="/tmp/emailmessage.txt"
+
+function ext-ip () { curl http://ipecho.net/plain; echo; }
+
+#read old ip 
+
+NEWIP=$(ext-ip)
+
+#check if log file is there
+touch $LOG || exit
+
+OLDIP=$(head -n 1 $LOG)
+
+#if ip not updated skip 
+if [ "$NEWIP" != "$OLDIP" ]
+then
+	echo "New IP detected"
+	echo $NEWIP > $LOG
+	#send email	
+	echo "New IP is: $NEWIP"> $EMAILMESSAGE
+	echo "Date: "$NOW >>$EMAILMESSAGE
+
+	$MAIL -s "$SUBJECT" "$EMAIL" < $EMAILMESSAGE
+else
+	echo "IP not updated"
+fi
+
